f2b32581165f45924c7f9b2bbbc894018e7de29c
d3nnyyy <d3nnn41k@gmail.com>
2023-06-26 18:03:38 +0300

    add counter for done tasks today

--- to-do-list-frontend/src/layout/Navbar.js
+++ to-do-list-frontend/src/layout/Navbar.js
@@ -1,7 +1,20 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 
 export default function Navbar() {
+
+     const [taskCount, setTaskCount] = useState(0);
+
+     useEffect(() => {
+          fetchTaskCount();
+     }, []);
+
+     const fetchTaskCount = async () => {
+          const result = await axios.get('http://localhost:8080/todo/done-today');
+          setTaskCount(result.data.length);
+     };
+
      return (
           <div>
                <nav className="navbar navbar-expand-lg navbar-dark" style={{ backgroundColor: '#303030', boxShadow: '0px 2px 4px rgba(0, 0, 0, 0.5)' }}>
@@ -10,9 +23,12 @@ export default function Navbar() {
                          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                               <span className="navbar-toggler-icon"></span>
                          </button>
+                         <div>
+                              <span className="text-light">Done Today: {taskCount}</span>
+                         </div>
                          <div className='ms-2'>
-                              <Link className="btn btn-outline-light me-2" to="/today">Today</Link>
-                              <Link className="btn btn-outline-light" to="/addTask">Add task</Link>
+                              <Link className="btn btn-outline-info me-4" to="/today">Today</Link>
+                              <Link className="btn btn-outline-warning me-2" to="/addTask">Add task</Link>
                          </div>
                     </div>
                </nav>
