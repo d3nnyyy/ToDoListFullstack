788995ee6983bbc9cbb87fe6d357b1d3cc8b56b4
d3nnyyy <d3nnn41k@gmail.com>
2023-06-27 12:29:52 +0300

    redesigned navbar a bit

--- to-do-list-frontend/src/layout/Navbar.js
+++ to-do-list-frontend/src/layout/Navbar.js
@@ -1,26 +1,68 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import { AiOutlineMenu, AiOutlineSearch, AiOutlineHome, AiOutlineCheckCircle } from 'react-icons/ai';
+import { RiTodoLine } from 'react-icons/ri';
+import { IoMdAdd } from 'react-icons/io';
 
 export default function Navbar({ taskCount }) {
+     const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+     const toggleMenu = () => {
+          setIsMenuOpen(!isMenuOpen);
+     };
 
      return (
-          <div>
-               <nav className="navbar navbar-expand-lg navbar-dark" style={{ backgroundColor: '#303030', boxShadow: '0px 2px 4px rgba(0, 0, 0, 0.5)' }}>
-                    <div className="container-fluid">
-                         <Link className="navbar-brand text-light" to="/">ToDo</Link>
-                         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                              <span className="navbar-toggler-icon"></span>
-                         </button>
-                         <div>
-                              <span className="text-light">Done Today: {taskCount}</span>
-                         </div>
-                         <div className='ms-2'>
-                              <Link className="btn btn-outline-info me-2" to="/done-today">Done Today</Link>
-                              <Link className="btn btn-outline-info me-4" to="/today">To do Today</Link>
-                              <Link className="btn btn-outline-warning me-2" to="/addTask">Add task</Link>
+          <nav
+               className="navbar navbar-expand-lg navbar-dark bg-dark"
+               style={{ boxShadow: '0px 2px 4px rgba(0, 0, 0, 0.5)' }}
+          >
+               <div className="container">
+                    <button
+                         className="navbar-toggler text-light"
+                         type="button"
+                         onClick={toggleMenu}
+                         aria-label="Toggle navigation"
+                    >
+                         <AiOutlineMenu />
+                    </button>
+                    <div className={`collapse navbar-collapse ${isMenuOpen ? 'show' : ''}`}>
+                         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                              <li className="nav-item">
+                                   <Link className="nav-link" to="/">
+                                        <AiOutlineHome className="text-light" />
+                                        Home
+                                   </Link>
+                              </li>
+                              <li className="nav-item">
+                                   <Link className="nav-link" to="/tasks">
+                                        <RiTodoLine className="text-light" />
+                                        Tasks
+                                   </Link>
+                              </li>
+                              <li className="nav-item">
+                                   <Link className="nav-link" to="/today">
+                                        <AiOutlineCheckCircle className="text-light" />
+                                        Today
+                                   </Link>
+                              </li>
+                         </ul>
+                         <div className="d-flex align-items-center">
+                              <span className="text-light me-3">Done Today: {taskCount}</span>
+                              <form className="d-flex">
+                                   <div className="input-group">
+                                        <input className="form-control" type="search" placeholder="Search" aria-label="Search" />
+                                        <button className="btn btn-light text-dark" type="submit">
+                                             <AiOutlineSearch />
+                                        </button>
+                                   </div>
+                              </form>
+
                          </div>
                     </div>
-               </nav>
-          </div>
+               </div>
+               <Link className="btn btn-transparent me-4 fs-4" to="/addTask">
+                    <IoMdAdd className="text-light" />
+               </Link>
+          </nav>
      );
 }
