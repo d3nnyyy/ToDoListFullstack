70ad2e3429c6b0feef04adb5f5f2463776f99bd9
d3nnyyy <d3nnn41k@gmail.com>
2023-07-04 18:59:05 +0300

    added cleanup system for done tasks

--- ToDoListBackend/src/main/java/ua/dtsebulia/ToDoListBackend/repository/ToDoItemRepository.java
+++ ToDoListBackend/src/main/java/ua/dtsebulia/ToDoListBackend/repository/ToDoItemRepository.java
@@ -8,4 +8,6 @@ import java.util.List;
 public interface ToDoItemRepository extends JpaRepository<ToDoItem, Integer> {
     List<ToDoItem> findItemsByDeadline(LocalDate today);
     List<ToDoItem> findItemsByDoneAndCompletedDate(boolean b, LocalDate today);
+
+    void deleteByDoneAndCompletedDateBefore(boolean b, LocalDate oneDayAgo);
 }
